bf2c043643703f64955d3c198feb9df915a6302c
Jefferson <jeffersonmauricioreis@gmail.com>
2025-04-10 16:32:38 -0300

    v2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def verificar_resposta():
     return jsonify({"erro": "Pergunta não encontrada"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
